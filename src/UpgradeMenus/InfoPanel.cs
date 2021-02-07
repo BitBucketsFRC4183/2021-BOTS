8303e65e8b8c1183b0a6c4e205b0a0a887a6fa61
Saptarshi Mallick <sapmallick@gmail.com>
2021-02-07 14:18:16 -0700

    Added InfoPanel progress bar logic
    Can't do much more here until the Player object is made

--- src/UpgradeMenus/InfoPanel.cs
+++ src/UpgradeMenus/InfoPanel.cs
@@ -3,5 +3,16 @@ using System;
 
 public class InfoPanel : Control
 {
-    
+    public UpgradeCost cost { get; set; }
+
+    public override void _Ready()
+    {
+        for (int i = 0; i < UpgradeCost.NumResourceTypes; i++)
+        {
+            var bar = GetNode<Panel>("Panel").GetNode<VBoxContainer>("Vbox").GetNode<ProgressBar>("Resource" + (i + 1));
+
+            if (cost.Costs[i] == 0) bar.Visible = false;
+            else bar.MaxValue = cost.Costs[i];
+        }
+    }
 }
