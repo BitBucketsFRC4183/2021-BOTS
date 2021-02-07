9cb031ef91ad2bf1c9977dfa275a31479b5e228a
Saptarshi Mallick <sapmallick@gmail.com>
2021-02-07 09:46:42 -0700

    Converted menu node methods into private fields

--- UpgradeMenus/UpgradeMain.cs
+++ UpgradeMenus/UpgradeMain.cs
@@ -3,28 +3,24 @@ using System;
 
 public class UpgradeMain : MarginContainer
 {
+    private UpgradeShip shipMenu;
+    private UpgradeRover roverMenu;
+    
     public override void _Ready()
     {
-        ShipMenu().Visible = true;
-        RoverMenu().Visible = false;
+        this.shipMenu = GetNode<Panel>("Panel").GetNode<UpgradeShip>("ShipUpgradeMenu");
+        this.roverMenu = GetNode<Panel>("Panel").GetNode<UpgradeRover>("RoverUpgradeMenu");
+
+        this.shipMenu.Visible = true;
+        this.roverMenu.Visible = false;
     }
     
     public void OnSwitchMenuPressed()
     {
         //If the Ship Menu is open, open the Rover Menu and vice versa
-        bool isShipOpen = ShipMenu().Visible;
+        bool isShipOpen = this.shipMenu.Visible;
 
-        ShipMenu().Visible = !isShipOpen;
-        RoverMenu().Visible = isShipOpen;
-    }
-
-    private UpgradeShip ShipMenu()
-    {
-        return GetNode<Panel>("Panel").GetNode<UpgradeShip>("ShipUpgradeMenu");
-    }
-
-    private UpgradeRover RoverMenu()
-    {
-        return GetNode<Panel>("Panel").GetNode<UpgradeRover>("RoverUpgradeMenu");
+        this.shipMenu.Visible = !isShipOpen;
+        this.roverMenu.Visible = isShipOpen;
     }
 }
