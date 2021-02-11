f633aaefc181931ef0418f43356d92637e371718
Unknown <nitishbhamidipati@gmail.com>
2021-02-10 19:43:34 -0700

    Buttons that work!
    Added the "New Game" Button and "Exit" Button functionality that either switches to the spaceplanet scene or closes out the game when they press the exit button

---
+++ src/MainMenu.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public class MainMenu : MarginContainer
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        FindNode("NewGame").Connect("pressed", this, nameof(OnNewGamePressed));
+        FindNode("Continue").Connect("pressed", this, nameof(OnContinuePressed));
+        FindNode("Exit").Connect("pressed", this, nameof(OnExitPressed));
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+    void OnNewGamePressed()
+    {
+        GetTree().ChangeScene("res://src/Space/SpacePlanet.tscn");
+    }
+
+    void OnContinuePressed()
+    {
+        //
+    }
+
+    void OnExitPressed()
+    {
+        GetTree().Quit();
+    }
+}
