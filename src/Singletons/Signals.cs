5e7d89927d29c07e92c7932c63235664a54aa0c9
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-02-11 09:52:41 -0700

    HUD resource collecting progress bar

--- src/Singletons/Signals.cs
+++ src/Singletons/Signals.cs
@@ -8,6 +8,9 @@ public class Signals : Node
     [Signal] public delegate void OverlappingResource(GameResource res);
     [Signal] public delegate void NotOverlappingResource();
 
+    [Signal] public delegate void CollectingResource();
+    [Signal] public delegate void NotCollectingResource();
+
     public override void _Ready()
     {
         Instance = this;
