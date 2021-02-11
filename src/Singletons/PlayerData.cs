2000e8c86927931f235e2df67c702368e8a5a7b0
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-02-10 19:57:26 -0700

    Separate PlayerResources class to hold resources data

--- src/Singletons/PlayerData.cs
+++ src/Singletons/PlayerData.cs
@@ -1,19 +1,10 @@
 using Godot;
-using System.Collections.Generic;
-
 
 public class PlayerData : Node
 {
     public static PlayerData Instance;
 
-    public Dictionary<Enums.GameResources, int> resources = new Dictionary<Enums.GameResources, int>() {
-        {Enums.GameResources.Graprofium, 0},
-        {Enums.GameResources.Kamenium, 0},
-        {Enums.GameResources.Wooflowium, 0},
-        {Enums.GameResources.Efarcium, 0},
-        {Enums.GameResources.Xerocrium, 0},
-        {Enums.GameResources.Coopertonium, 0},
-    };
+    public PlayerResources resources = new PlayerResources();
 
     public override void _Ready()
     {
