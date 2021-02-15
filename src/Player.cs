1d1d0bb4965b69b3e621b9dcdbd0902ff5057d24
Unknown <nitishbhamidipati@gmail.com>
2021-02-15 08:01:19 -0700

    Aliens and bullets and Players Oh my!
    added bullets for the player to shoot by clicking left mouse button. Added alien scene and basic setup

--- src/Player.cs
+++ src/Player.cs
@@ -5,18 +5,21 @@ public class Player : KinematicBody2D
 {
     const int SPEED = 400;
     Vector2 motion = new Vector2();
-    
+
     AnimatedSprite sprite;
     Timer timer;
 
     bool overlappingResource = false;
     GameResource resource;
 
+    [Export]
+    public PackedScene BulletScene;
+
     public override void _Ready()
     {
         sprite = GetNode<AnimatedSprite>("AnimatedSprite");
         timer = GetNode<Timer>("ResourceCollectTimer");
-        
+
         Signals.Instance.Connect("OverlappingResource", this, nameof(OnOverlappingResource));
         Signals.Instance.Connect("NotOverlappingResource", this, nameof(OnNotOverlappingResource));
     }
@@ -24,11 +27,13 @@ public class Player : KinematicBody2D
     /////////
     // collecting resources code
 
-    void OnOverlappingResource(GameResource res) {
+    void OnOverlappingResource(GameResource res)
+    {
         overlappingResource = true;
         resource = res;
     }
-    void OnNotOverlappingResource() {
+    void OnNotOverlappingResource()
+    {
         overlappingResource = false;
         GD.Print("stopped collecting");
         resource = null;
@@ -38,37 +43,43 @@ public class Player : KinematicBody2D
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (@event.IsActionPressed("resource_collect")) {
-            if (overlappingResource) {
+        if (@event.IsActionPressed("resource_collect"))
+        {
+            if (overlappingResource)
+            {
                 timer.Start();
                 Signals.Instance.EmitSignal(nameof(Signals.CollectingResource));
             }
         }
 
-        if (@event.IsActionReleased("resource_collect")) {
+        if (@event.IsActionReleased("resource_collect"))
+        {
             GD.Print("stopped collecting");
             resource = null;
             timer.Stop();
             Signals.Instance.EmitSignal(nameof(Signals.NotCollectingResource));
-            
+
         }
     }
 
-    void _on_ResourceCollectTimer_timeout() {
+    void _on_ResourceCollectTimer_timeout()
+    {
         GD.Print("finished!");
-        if (resource != null) {
+        if (resource != null)
+        {
             PlayerData.Instance.resources[resource.type] += resource.amount;
             resource.QueueFree();
             resource = null;
             Signals.Instance.EmitSignal(nameof(Signals.NotCollectingResource));
         }
     }
-    
-    
+
+
     /////////
     // movement code
 
-    Vector2 CartesianToIsometric(Vector2 cartesian) {
+    Vector2 CartesianToIsometric(Vector2 cartesian)
+    {
         return new Vector2(
             cartesian.x - cartesian.y,
             (cartesian.x + cartesian.y) / 2
@@ -77,23 +88,36 @@ public class Player : KinematicBody2D
 
     public override void _Process(float delta)
     {
+        if (Input.IsActionJustPressed("shoot"))
+        {
+            Bullet bullet = BulletScene.Instance() as Bullet;
+            bullet.Position = Position;
+            bullet.Rotation = GetAngleTo(GetGlobalMousePosition());
+            GetTree().Root.AddChild(bullet);
+        }
         Vector2 direction = new Vector2();
 
         // Note: right now only 4-direction movement is implemented because 
         // we don't have sprites for 8-directional and it looks confusing
         // to re-add 8-direction, simlpy make all of these "+="
-        if (Input.IsActionPressed("player_up")) {
+        if (Input.IsActionPressed("player_up"))
+        {
             direction = Vector2.Up; // (0, -1)
             sprite.Animation = "NE";
-        } else if (Input.IsActionPressed("player_down")) {
+        }
+        else if (Input.IsActionPressed("player_down"))
+        {
             direction = Vector2.Down; // (0, 1)
             sprite.Animation = "SW";
         }
 
-        if (Input.IsActionPressed("player_left")) {
+        if (Input.IsActionPressed("player_left"))
+        {
             direction = Vector2.Left; // (-1, 0)
             sprite.Animation = "NW";
-        } else if (Input.IsActionPressed("player_right")) {
+        }
+        else if (Input.IsActionPressed("player_right"))
+        {
             direction = Vector2.Right; // (1, 0)
             sprite.Animation = "SE";
         }
