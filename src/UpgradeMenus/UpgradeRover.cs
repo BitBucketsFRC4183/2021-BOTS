7ea1045b3f10e779c4588ef98a1f9284e5e0fed6
Saptarshi Mallick <sapmallick@gmail.com>
2021-02-17 23:39:35 -0700

    Added basic panel layout to Rover Upgrade Scene

--- src/UpgradeMenus/UpgradeRover.cs
+++ src/UpgradeMenus/UpgradeRover.cs
@@ -3,5 +3,12 @@ using System;
 
 public class UpgradeRover : MarginContainer
 {
-    
+    public override void _Ready()
+    {
+        var bg = GetNode<ColorRect>("Background");
+        bg.GetNode<InfoPanel>("Speed").SetPanelType(InfoPanel.UpgradeType.ROVER, Enums.RoverUpgradeType.SPEED);
+        bg.GetNode<InfoPanel>("Tank").SetPanelType(InfoPanel.UpgradeType.ROVER, Enums.RoverUpgradeType.TANK);
+        bg.GetNode<InfoPanel>("Regen").SetPanelType(InfoPanel.UpgradeType.ROVER, Enums.RoverUpgradeType.REGEN);
+        bg.GetNode<InfoPanel>("Inventory").SetPanelType(InfoPanel.UpgradeType.ROVER, Enums.RoverUpgradeType.INVENTORY);
+    }
 }
