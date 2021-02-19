47c4fab67de185b72c25a3708153a22a1279e1d5
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-02-19 08:01:35 -0700

    Player has animated collisionpolygon now (#69)

---
+++ src/Space/SpaceShuttleArea.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public class SpaceShuttleArea : Area2D
+{
+    [Export]
+    PackedScene spaceScene;
+
+    public void _on_SpaceShuttleArea_body_entered(Node body)
+    {
+        SceneChanger.Instance.GoToScene(spaceScene);
+    }
+
+}
