6875e0a00cd380ebadfd0eb114b8ac9f326069bf
Saptarshi Mallick <sapmallick@gmail.com>
2021-02-19 17:37:41 -0700

    Added Ship Upgrade (Shields) to scene

--- src/UpgradeMenus/UpgradeShip.cs
+++ src/UpgradeMenus/UpgradeShip.cs
@@ -3,5 +3,11 @@ using System;
 
 public class UpgradeShip : MarginContainer
 {
-    
+    public override void _Ready()
+    {
+        var bg = GetNode<ColorRect>("Background");
+        bg.GetNode<InfoPanel>("Shields").SetPanelType(InfoPanel.UpgradeType.SHIP, Enums.RoverUpgradeType.NULL, Enums.ShipUpgradeType.SHIELDS);
+        
+        Signals.PublishUpgradesChangedEvent();
+    }
 }
