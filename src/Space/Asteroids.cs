efd518b7693b357b1fd8a0c061bddc9e7044fd74
Pranav Barla <pbarla003@gmail.com>
2021-02-20 16:32:11 -0700

    changes for asteroid

--- src/Space/Asteroids.cs
+++ src/Space/Asteroids.cs
@@ -10,6 +10,26 @@ public class Asteroids : RigidBody2D
     {
         
     }
+public class Main : Node
+{
+
+    [Export]
+    public PackedScene Asteroid;
+
+    private int _score;
+
+    private Random _random = new Random();
+
+    public override void _Ready()
+    {
+    }
+
+
+    private float RandRange(float min, float max)
+    {
+        return (float)_random.NextDouble() * (max - min) + min;
+    }
+}
 
 
 }
