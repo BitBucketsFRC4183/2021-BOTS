f42e760fb4a9ac3b3b5c0c72832551033985ff74
Pranav Barla <pbarla003@gmail.com>
2021-02-20 17:04:31 -0700

    added asteroids to space

--- src/Space/Asteroids.cs
+++ src/Space/Asteroids.cs
@@ -3,33 +3,11 @@ using System;
 
 public class Asteroids : RigidBody2D
 {
-    
 
- 
-    public override void _Ready()
+
+
+    public override void _PhysicsProcess(float delta)
     {
-        
+        ApplyTorqueImpulse(100);
     }
-public class Main : Node
-{
-
-    [Export]
-    public PackedScene Asteroid;
-
-    private int _score;
-
-    private Random _random = new Random();
-
-    public override void _Ready()
-    {
-    }
-
-
-    private float RandRange(float min, float max)
-    {
-        return (float)_random.NextDouble() * (max - min) + min;
-    }
-}
-
-
 }
