edb48c2fe6a8b0dccad7e66bdece806955e49d6f
Saptarshi Mallick <sapmallick@gmail.com>
2021-02-23 08:33:30 -0700

    Added Tank and Speed GUI

--- src/UpgradeMenus/UpgradeShip.cs
+++ src/UpgradeMenus/UpgradeShip.cs
@@ -3,15 +3,19 @@ using System;
 
 public class UpgradeShip : MarginContainer
 {
-    private InfoPanel Shields;
+    private InfoPanel Shields, Tank, Speed;
     public override void _Ready()
     {
         Signals.UpgradesChanged += CheckVisibility;
         
         var bg = GetNode<ColorRect>("Background");
         Shields = bg.GetNode<InfoPanel>("Shields");
+        Tank = bg.GetNode<InfoPanel>("Tank");
+        Speed = bg.GetNode<InfoPanel>("Speed");
         
         Shields.SetPanelType(InfoPanel.UpgradeType.SHIP, Enums.RoverUpgradeType.NULL, Enums.ShipUpgradeType.SHIELDS);
+        Tank.SetPanelType(InfoPanel.UpgradeType.SHIP, Enums.RoverUpgradeType.NULL, Enums.ShipUpgradeType.TANK);
+        Speed.SetPanelType(InfoPanel.UpgradeType.SHIP, Enums.RoverUpgradeType.NULL, Enums.ShipUpgradeType.SPEED);
         
         Signals.PublishUpgradesChangedEvent();
     }
@@ -24,5 +28,7 @@ public class UpgradeShip : MarginContainer
     private void CheckVisibility()
     {
         Shields.Visible = !PlayerData.Instance.IsUpgradeMaxLevel(Enums.RoverUpgradeType.NULL, Shields.shipUpgrade);
+        Tank.Visible = !PlayerData.Instance.IsUpgradeMaxLevel(Enums.RoverUpgradeType.NULL, Tank.shipUpgrade);
+        Speed.Visible = !PlayerData.Instance.IsUpgradeMaxLevel(Enums.RoverUpgradeType.NULL, Speed.shipUpgrade);
     }
 }
